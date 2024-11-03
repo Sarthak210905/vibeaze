2302dc2884f8c69a1d797c5745d553bac947334f
Sarthak210905 <sarthakchoukse230608@acropolis.in>
2024-11-03 13:35:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 //create a separate file for this and then import/use that file here
 
 mongoose
-  .connect("db_url")
+  .connect( "mongodb+srv://viberaze:sarthak21choukse@cluster0.uohxd.mongodb.net/")
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
